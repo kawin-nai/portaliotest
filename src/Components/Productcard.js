5701a7381558c41cad85b80d27e3c5f461c4568a
Kawin Naipongprasit <qazseee@gmail.com>
2021-11-16 17:03:57 +0800

    Actual Changes
    Create product pages, product cards.

---
+++ src/Components/Productcard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../sass/format.scss'
+
+function Productcard(props) {
+    return (
+        <div className="card">
+            <div className="cardbody">
+                <img className="cardimage" src={props.img} width="200" height ="200"/>
+                <h2 className="cardtitle">{props.title}</h2> 
+                <p className="carddescription">{props.desc}</p>           
+            </div>
+            <button className="cardbutton">Pledge</button>
+        </div>
+    )
+}
+
+export default Productcard
---
+++ src/Pages/Productpage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Productcard from '../Components/Productcard'
+import '../sass/format.scss'
+
+function Productpage() {
+    return (
+        <div>
+            <div className="topbar">
+                <button className="topbarbutton">Create new project</button>
+            </div>
+            <h2>Products</h2>
+            <div className="wrapper">
+                <Productcard img="https://images.unsplash.com/photo-1637008336770-b95d637fd5fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1528&q=80"
+                title="Test" desc="This is a test description "/>
+                <Productcard img="https://images.unsplash.com/photo-1637002058121-7f3fde498f16?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+                title="Cookies" desc="This is a cookie" />
+                <Productcard img="https://images.unsplash.com/photo-1637002058121-7f3fde498f16?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+                title="Cookies" desc="This is a cookie" />
+                <Productcard img="https://images.unsplash.com/photo-1637002058121-7f3fde498f16?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+                title="Cookies" desc="This is a cookie" />
+            </div>
+        </div>
+        
+    )
+}
+
+export default Productpage
